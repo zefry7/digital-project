a3816d8f878cf50d9a602354cf21787d69ad272d
nikita petrov <petrov.nikita702@mail.ru>
2024-07-18 16:27:16 +0300

    адаптив блока footer

--- src/content/Layout/Footer.tsx
+++ src/content/Layout/Footer.tsx
@@ -7,34 +7,34 @@ function Footer() {
 
     return (
         <footer className={"bg-blackBg"}>
-            <div className={"flex text-white justify-between font-TTM padding-content py-[60px] max-w-[1440px] mx-auto"}>
+            <div className={"grid grid-cols-4 gap-y-[30px] gap-x-[15px] text-white font-TTM padding-content py-[60px] max-w-[1440px] mx-auto max-laptop:grid-cols-2 max-[425px]:grid-cols-1"}>
                 <div className="">
                     <img src={data?.logo?.src} alt={data?.logo?.alt} />
                 </div>
                 <div className={""}>
-                    <p className={"mb-[25px] font-TTB"}>Информация</p>
+                    <p className={"footer-title"}>Информация</p>
                     <ul className={"flex flex-col gap-[16px]"}>
                         {data?.menu?.map((item, i) => (
-                            <li key={i}>
-                                <a href="" className={"text-[14px]"}>{item}</a>
+                            <li key={i} className={"text-[14px]/[1]"}>
+                                <a href="" className={""}>{item}</a>
                             </li>
                         ))}
                     </ul>
                 </div>
                 <div className="">
-                    <p className={"mb-[25px] font-TTB"}>Контакты</p>
+                    <p className={"footer-title"}>Контакты</p>
                     <address className={"flex flex-col gap-[16px] not-italic"}>
                         {data?.contact?.map((item, i) => (
-                            <a href="" className={"footer-contact"}>{item}</a>
+                            <a href="" className={"footer-contact text-[14px]/[1]"}>{item}</a>
                         ))}
                     </address>
                 </div>
                 <div className="">
-                    <p className={"mb-[25px] font-TTB"}>Социальные сети</p>
-                    <ul className={"flex gap-[40px]"}>
+                    <p className={"footer-title"}>Социальные сети</p>
+                    <ul className={"flex gap-[30px] max-laptop:gap-[25px]"}>
                         {data?.socials?.map((item, i) => (
                             <li key={i}>
-                                <a href="" className={"text-[14px]"}>
+                                <a href="" className={"text-[14px] h-[22px] w-[22px] flex items -centerjustify-center"}>
                                     <img src={item?.src} alt={item?.alt} />
                                 </a>
                             </li>
@@ -43,7 +43,7 @@ function Footer() {
                 </div>
             </div>
             <div className={"py-[24px] border-t-[1px] border-t-[#C8C8C880]"}>
-                <p className={"text-[#C8C8C880] text-center"}>{data?.copyright}</p>
+                <p className={"text-[#C8C8C880] text-center padding-content"}>{data?.copyright}</p>
             </div>
         </footer>
     )
