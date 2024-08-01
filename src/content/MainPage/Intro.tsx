8f4494e07ad18bc8744067c57516af32072423ee
nikita petrov <petrov.nikita702@mail.ru>
2024-08-02 02:04:12 +0300

    Правки v1 02_08

--- src/content/MainPage/Intro.tsx
+++ src/content/MainPage/Intro.tsx
@@ -5,7 +5,7 @@ function Intro() {
     const data = useContext(DataProvider)?.mainPage
 
     return <section className={"mb-[120px] max-laptop:mb-[100px] max-tablet:mb-[0px]"}>
-        <div className={"tablet:padding-content relative flex items-center gap-[60px] max-tablet:space-x-[0px] max-tablet:flex-col max-laptop:gap-[40px] h-[840px]"}>
+        <div className={"tablet:padding-content relative flex items-center gap-[60px] max-tablet:space-x-[0px] max-tablet:flex-col max-laptop:gap-[40px] max-h-[840px]"}>
             <h1 className={"intro__title max-laptop:top-[20px]"}>
                 <span className={"font-TTL text-greyLight max-tablet:mr-[6px] max-tablet:text-white max-tablet:font-TTM"}>{data?.intro?.title[0]}</span>
                 {data?.intro?.title[1]}
